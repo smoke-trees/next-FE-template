0462dc1afc6153dcb52dd673aaf981fa19d9b55b
Adarsh Singh <adarsh@smoketrees.in>
2025-02-09 01:38:16 +0530

    feat: react compiler and eslint for it

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,15 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import reactCompiler from "eslint-plugin-react-compiler"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript"), reactCompiler.configs.recommended]
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    reactCompiler: true,
+  },
+}
 
 export default nextConfig;
