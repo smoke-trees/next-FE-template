70e7e68d647d8207636b35bf2104abc6e3a12cfc
Adarsh Singh <adarshmanoj.05.as@gmail.com>
2025-11-25 14:21:22 +0530

    feat: data provider

--- src/utils/authFetch.ts
+++ src/utils/authFetch.ts
@@ -1,11 +1,12 @@
 import { getAuthToken } from '@/dataFetching/loginHandler'
 
-export async function authFetch(url: string, options: RequestInit) {
+export async function authFetch(url: string, options?: RequestInit) {
+	if(!options) options = {}
 	const authToken = await getAuthToken()
 	if (authToken.token) {
 		options.headers = {
 			...options.headers,
-			Authorization: authToken.token
+			Authorization: `Bearer ${authToken.token}`
 		}
 	}
 	return fetch(url, options)
