a5f77f5f5cf9823cffa8afcd284bb9dfa28d7d86
Adarsh Singh <adarshmanoj.05.as@gmail.com>
2025-11-26 15:43:55 +0530

    fix: auth fetch

--- src/utils/authFetch.ts
+++ src/utils/authFetch.ts
@@ -2,6 +2,17 @@ import { getAuthToken } from '@/dataFetching/loginHandler'
 
 export async function authFetch(url: string, options?: RequestInit) {
 	if (!options) options = {}
+	if(options.body && typeof options.body === 'string') {
+		if(options.body[0] === '{') {
+			if(options.headers) {
+				(options.headers as any)['Content-Type'] = 'application/json'
+			} else {
+				options.headers = {
+					'Content-Type': 'application/json'
+				}
+			}
+		}
+	}
 	const authToken = await getAuthToken()
 	if (authToken.token) {
 		options.headers = {
@@ -11,3 +22,4 @@ export async function authFetch(url: string, options?: RequestInit) {
 	}
 	return fetch(url, options)
 }
+
